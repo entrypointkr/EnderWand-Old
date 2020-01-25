50b261a4eb67220ab620fc44b2daeecaae0d12dc
EntryPoint <entrypointkr@gmail.com>
2020-01-25 18:46:43 +0900

    Refactoring ItemStacks

---
+++ modules/bukkit/src/main/kotlin/kr/entree/enderwand/bukkit/item/ItemMetas.kt
@@ -0,0 +1,31 @@
+package kr.entree.enderwand.bukkit.item
+
+import kr.entree.enderwand.bukkit.message.colorize
+import org.bukkit.ChatColor
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.ItemMeta
+
+/**
+ * Created by JunHyung Lim on 2020-01-25
+ */
+inline fun ItemStack.meta(configure: ItemMeta.() -> Unit) {
+    val meta = itemMeta
+    if (meta != null) {
+        meta.configure()
+        itemMeta = meta
+    }
+}
+
+fun ItemMeta.setName(name: String) = setDisplayName(name.colorize())
+
+fun ItemMeta.setLore(vararg lore: String) = setLore(lore.map { ChatColor.WHITE.toString() + it.colorize() })
+
+inline fun ItemMeta.lore(builder: MutableList<String>.() -> Unit) =
+    setLore(mutableListOf<String>().apply(builder).map { it.colorize() })
+
+val ItemStack.displayName get() = itemMeta?.displayName ?: ""
+
+val ItemMeta.modelData
+    get() = if (hasCustomModelData()) {
+        customModelData
+    } else null
--- modules/bukkit/src/main/kotlin/kr/entree/enderwand/bukkit/item/ItemStacks.kt
+++ modules/bukkit/src/main/kotlin/kr/entree/enderwand/bukkit/item/ItemStacks.kt
@@ -1,10 +1,7 @@
 package kr.entree.enderwand.bukkit.item
 
-import kr.entree.enderwand.bukkit.message.colorize
-import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
-import org.bukkit.inventory.meta.ItemMeta
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
@@ -13,26 +10,9 @@ import kotlin.contracts.contract
  */
 fun Material.toItem(amount: Int = 1) = ItemStack(this, amount)
 
-inline fun ItemStack.meta(configure: ItemMeta.() -> Unit) {
-    val meta = itemMeta
-    if (meta != null) {
-        meta.configure()
-        itemMeta = meta
-    }
-}
-
 inline fun item(material: Material, configure: ItemStack.() -> Unit = {}) = material.toItem().apply(configure)
 
 inline fun item(item: ItemStack, configure: ItemStack.() -> Unit = {}) = ItemStack(item).apply(configure)
-
-fun ItemMeta.setName(name: String) = setDisplayName(name.colorize())
-
-fun ItemMeta.setLore(vararg lore: String) = setLore(lore.map { ChatColor.WHITE.toString() + it.colorize() })
-
-inline fun ItemMeta.lore(builder: MutableList<String>.() -> Unit) =
-    setLore(mutableListOf<String>().apply(builder).map { it.colorize() })
-
-val ItemStack.displayName get() = itemMeta?.displayName ?: ""
 
 @UseExperimental(ExperimentalContracts::class)
 fun ItemStack?.isAir(): Boolean {
