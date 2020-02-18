4ea98c28742adece854d72665f73eea2fef6f7fd
EntryPoint <entrypointkr@gmail.com>
2020-02-18 11:40:11 +0900

    Implement functional event listener experimentally

--- modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/event/Events.kt
+++ modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/event/Events.kt
@@ -1,10 +1,13 @@
 package kr.entree.enderwand.bukkit.event
 
+import org.bukkit.Bukkit
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.player.PlayerEvent
+import org.bukkit.plugin.Plugin
 
 /**
  * Created by JunHyung Lim on 2020-01-06
@@ -23,4 +26,18 @@ fun Event.findPlayer() =
             player
         }
         else -> null
+    }
+
+inline fun <reified T : Event> Plugin.on(
+    priority: EventPriority = EventPriority.NORMAL,
+    ignoreCancelled: Boolean = false,
+    crossinline receiver: T.() -> Unit
+) =
+    FunctionalEventExecutor(ignoreCancelled) { _, event ->
+        if (event is T) {
+            receiver(event)
+        }
+    }.also { executor ->
+        Bukkit.getPluginManager()
+            .registerEvent(T::class.java, executor, priority, executor, this)
     }
---
+++ modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/event/FunctionalEventExecutor.kt
@@ -0,0 +1,24 @@
+package kr.entree.enderwand.bukkit.event
+
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.plugin.EventExecutor
+
+/**
+ * Created by JunHyung Lim on 2020-02-18
+ */
+class FunctionalEventExecutor(
+    private val ignoreCancelled: Boolean,
+    private val function: (Listener, Event) -> Unit
+) : EventExecutor, Listener {
+    override fun execute(listener: Listener, event: Event) {
+        if (event is Cancellable && event.isCancelled && ignoreCancelled) {
+            return
+        }
+        function(listener, event)
+    }
+
+    fun unregister() = HandlerList.unregisterAll(this)
+}
