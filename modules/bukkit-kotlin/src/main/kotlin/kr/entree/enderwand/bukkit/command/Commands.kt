63437ddc0ea223e77813c98d1b8c6b26a3e51f89
EntryPoint <entrypointkr@gmail.com>
2020-02-18 12:40:18 +0900

    Add player property in CommandContext<BukkitSender, T>

--- modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/command/Commands.kt
+++ modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/command/Commands.kt
@@ -1,9 +1,11 @@
 package kr.entree.enderwand.bukkit.command
 
+import kr.entree.enderwand.bukkit.player.toPlayerOrThrow
 import kr.entree.enderwand.command.CommandBuilder
 import kr.entree.enderwand.command.CommandContext
 import kr.entree.enderwand.command.command
 import kr.entree.enderwand.command.withHelper
+import org.bukkit.entity.Player
 
 /**
  * Created by JunHyung Lim on 2019-12-20
@@ -13,3 +15,5 @@ typealias BukkitExecutor<T> = CommandContext<BukkitSender, T>.() -> Unit
 inline fun bukkitCommand(
     configure: CommandBuilder<BukkitSender>.() -> Unit
 ) = command(configure).withHelper(BukkitCommandHelper())
+
+val <T> CommandContext<BukkitSender, T>.player: Player get() = sender.toPlayerOrThrow()
