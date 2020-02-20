168828c563b78064c57de6b472f65e1c0804d2f6
EntryPoint <entrypointkr@gmail.com>
2020-02-20 14:03:15 +0900

    Do not use nullable call

--- modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/player/OfflinePlayers.kt
+++ modules/bukkit-kotlin/src/main/kotlin/kr/entree/enderwand/bukkit/player/OfflinePlayers.kt
@@ -4,14 +4,16 @@ import kr.entree.enderwand.bukkit.coroutine.awaitJoin
 import kr.entree.enderwand.bukkit.coroutine.launch
 import kr.entree.enderwand.bukkit.enderWand
 import kr.entree.enderwand.bukkit.sender.tell
+import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 
 /**
  * Created by JunHyung Lim on 2020-02-19
  */
 fun OfflinePlayer.tellOrLater(message: String) {
-    if (player?.isOnline == true) {
-        player?.tell(message)
+    val onlinePlayer = Bukkit.getPlayer(uniqueId)
+    if (onlinePlayer != null) {
+        onlinePlayer.tell(message)
     } else {
         enderWand.launch {
             awaitJoin(uniqueId).tell(message)
